ffafbae8d8b64ca7227131dac3a7cdc4eeee7cf9
Mukola08 <163895904+Mukola08@users.noreply.github.com>
2025-08-29 11:13:21 +0300

    full

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import { plus, mines, multiplay } from "./js/my-script";
+console.log(plus(9, 7));
+console.log(multiplay(9, 7));
+console.log(mines(9, 4));
---
+++ src/js/my-script.js
@@ -0,0 +1,3 @@
+export const plus = (a, b) => a + b;
+export const mines = (a, b) => a - b;
+export const multiplay = (a, b) => a * b;
